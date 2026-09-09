1885b0d14cc440869282cd583752f6590e4c3ed8
Johnathon Forker <johnf0rks303@pm.me>
2026-09-09 08:51:27 -0700

    installer

--- lzinstall.sh
+++ lzinstall.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+here="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)" || exit 1
 target="$HOME/.local/bin"
 result="$HOME/.local/bin/pycon"
 
-if [[ ! "$target" == "$HOME/.local/bin" ]]; then
-  mkdir -p "$target"; ln -sf "$PWD"/python_pycon_source.py "$result" \
-  	export PATH="$HOME/.local/bin:$PATH" >> ~/.bashrc && export PATH="$HOME/.local/bin:$PATH" >> ~/.zshrc
-  else
-  	  ln -sf "$PWD"/python_pycon_source.py "$result"
+mkdir -p "$target" || exit 1
+
+if [[ -e "$result" || -L "$result" ]]; then
+  echo "pycon is already installed"
+  elif
+  [[ -e "$target" || -L "$result" ]]; then
+  ln -sf "$here"/python_pycon_source.py "$result"
+fi
+
+if [[ ":$PATH:" != *":$target:"* ]]; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
 fi
